c653438790e786450a2e45a04f6010fc4a91ba21
Solhvemjsun <solhvemjsun@github.com>
2025-05-13 17:25:32 -0700

    Create C dev environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Sol's OS, Fiat Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+  };
+
+  outputs = { nixpkgs, nixvim, ... }: let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+
+    devShells."x86_64-linux" = {
+      "C" = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          git
+          gcc
+          cmake
+        ];
+        shellHook = ''
+          echo "Fiat Nix!"
+          vim ./C
+        '';
+      };
+    };
+
+  };
+}
