c5c73c3a9a13c96b64bca88a9bfbf08f1be105b0
Solhvemjsun <Solhvemjsun@gmail.com>
2025-08-25 00:53:22 -0700

    Rustbook 4.2

--- Rust/ownership/src/main.rs
+++ Rust/ownership/src/main.rs
@@ -1,8 +1,36 @@
 fn main() {
-    let stringvar = "hello";
-    {
-        let stringvar = "world";
-        println!("{stringvar}");
-    }
-    println!("{stringvar}");
-}
+    let s1 = String::from("hello");
+    let mut s2 = s1.clone();
+    println!("{s1}, world!");
+    println!("{s2}, world!");
+    s2 = String::from("ahoy");
+    println!("{s2}, world!");
+
+    let x = 5;
+    let y = x;
+    println!("x = {x}, y = {y}");
+
+    let s = String::from("hello"); // s comes into scope
+
+    takes_ownership(s); // s's value moves into the function...
+    // ... and so is no longer valid here
+
+    let x = 5; // x comes into scope
+
+    makes_copy(x); // Because i32 implements the Copy trait,
+    // x does NOT move into the function,
+    // so it's okay to use x afterward.
+
+} // Here, x goes out of scope, then s. However, because s's value was moved,
+// nothing special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{some_integer}");
+} // Here, some_integer goes out of scope. Nothing special happens.
